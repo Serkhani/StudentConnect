2d0bd83a040dbc56ad32a567fdd6298d97e3f0b0
Serkhani <deviper03@gmail.com>
2023-09-30 12:58:38 +0100

    added auth page

--- flutter/studentconnect/lib/app/routes/app_pages.dart
+++ flutter/studentconnect/lib/app/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 
+import '../modules/auth/bindings/auth_binding.dart';
+import '../modules/auth/views/auth_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 
@@ -15,6 +17,11 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.AUTH,
+      page: () => const AuthView(),
+      binding: AuthBinding(),
     ),
   ];
 }
