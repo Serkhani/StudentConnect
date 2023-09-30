0dca23d02acb15c3badc891054e28b267a85efb8
Serkhani <deviper03@gmail.com>
2023-09-30 12:45:46 +0100

    initialized springboot

---
+++ springboot/studentconnect/src/main/java/com/app/studentconnect/StudentconnectApplication.java
@@ -0,0 +1,13 @@
+package com.app.studentconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentconnectApplication.class, args);
+	}
+
+}
---
+++ springboot/studentconnect/src/test/java/com/app/studentconnect/StudentconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.studentconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
