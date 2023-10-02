dcad3a32598ae97c015404b1a07d10b4109629cf
Serkhani <deviper03@gmail.com>
2023-10-02 18:56:48 +0100

    added login button

--- flutter/studentconnect/lib/app/modules/auth/views/components/login_btn.dart
+++ flutter/studentconnect/lib/app/modules/auth/views/components/login_btn.dart
@@ -5,6 +5,28 @@ class LoginBtn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+          fixedSize: const Size(double.infinity, 55.0),
+          backgroundColor: const Color(0xff2D4990)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            'assets/gm.png',
+            width: 30.0,
+            height: 26.5,
+          ),
+          const Text(
+            'Log in with your Google account',
+            style: TextStyle(
+              fontSize: 16.0,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
