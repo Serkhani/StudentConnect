20e3735bb04043dbb54749ddd7c924e776100aa3
Serkhani <deviper03@gmail.com>
2023-10-02 17:55:33 +0100

    updated sign up view  and added auth view

--- flutter/studentconnect/lib/app/modules/signup/views/signup_view.dart
+++ flutter/studentconnect/lib/app/modules/signup/views/signup_view.dart
@@ -8,15 +8,402 @@ class SignupView extends GetView<SignupController> {
   const SignupView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('SignupView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SignupView is working',
-          style: TextStyle(fontSize: 20),
+    return  SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+          children: [
+            Container(
+              width: 414,
+              height: 896,
+              clipBehavior: Clip.antiAlias,
+              decoration: const BoxDecoration(color: Color(0xFFD4F2E7)),
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 20,
+                    top: 225,
+                    child: SizedBox(
+                      width: 340,
+                      height: 55,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: Container(
+                              width: 340,
+                              height: 55,
+                              decoration: ShapeDecoration(
+                                color: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5)),
+                              ),
+                            ),
+                          ),
+                          const Positioned(
+                            left: 14,
+                            top: 18,
+                            child: SizedBox(
+                              width: 312,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: 'Enter your first name',
+                                  hintStyle: TextStyle(
+                                    color: Colors.black26,
+                                  ),
+                                ),
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 20,
+                    top: 311,
+                    child: SizedBox(
+                      width: 340,
+                      height: 55,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: Container(
+                              width: 340,
+                              height: 55,
+                              decoration: ShapeDecoration(
+                                color: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5)),
+                              ),
+                            ),
+                          ),
+                          const Positioned(
+                            left: 14,
+                            top: 18,
+                            child: SizedBox(
+                              width: 312,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Last name',
+                                    hintStyle: TextStyle(color: Colors.black26)),
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 20,
+                    top: 397,
+                    child: SizedBox(
+                      width: 340,
+                      height: 55,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: Container(
+                              width: 340,
+                              height: 55,
+                              decoration: ShapeDecoration(
+                                color: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5)),
+                              ),
+                            ),
+                          ),
+                          const Positioned(
+                            left: 14,
+                            top: 18,
+                            child: SizedBox(
+                              width: 312,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Email/phone number',
+                                    hintStyle: TextStyle(color: Colors.black26)),
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 20,
+                    top: 483,
+                    child: SizedBox(
+                      width: 340,
+                      height: 55,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: Container(
+                              width: 340,
+                              height: 55,
+                              decoration: ShapeDecoration(
+                                color: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5)),
+                              ),
+                            ),
+                          ),
+                          const Positioned(
+                            left: 14,
+                            top: 18,
+                            child: SizedBox(
+                              width: 312,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Password',
+                                    hintStyle: TextStyle(color: Colors.black26)),
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 20,
+                    top: 570,
+                    child: SizedBox(
+                      width: 340,
+                      height: 55,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: Container(
+                              width: 340,
+                              height: 55,
+                              decoration: ShapeDecoration(
+                                color: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5)),
+                              ),
+                            ),
+                          ),
+                          const Positioned(
+                            left: 14,
+                            top: 18,
+                            child: SizedBox(
+                              width: 312,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                    hintText: 'Confirm password',
+                                    hintStyle: TextStyle(color: Colors.black26)),
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.w400,
+                                  height: 0,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Positioned(
+                    left: 155,
+                    top: 92,
+                    child: Text(
+                      'Welcome!',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 24,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  const Positioned(
+                    left: 37,
+                    top: 150,
+                    child: Text(
+                      'Sign up for StudentsConnect',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
+                        height: 0,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 109,
+                    top: 736,
+                    child: SizedBox(
+                      width: 196,
+                      height: 55,
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            left: 0,
+                            top: 0,
+                            child: Container(
+                              width: 196,
+                              height: 55,
+                              decoration: ShapeDecoration(
+                                color: const Color(0xFF2D4890),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const Positioned(
+                            left: 61,
+                            top: 15,
+                            child: Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w600,
+                                height: 0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Positioned(
+                    left: 67,
+                    top: 645,
+                    child: SizedBox(
+                      width: 305,
+                      child: Text.rich(
+                        TextSpan(
+                          children: [
+                            TextSpan(
+                              text:
+                                  'By clicking on ‘sign up’, you’re agreeing to the StudentConnects ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 11,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w400,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: 'Terms of Service',
+                              style: TextStyle(
+                                color: Color(0xFF2D4890),
+                                fontSize: 11,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w500,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: ' and ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 11,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w400,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: 'Privacy',
+                              style: TextStyle(
+                                color: Color(0xFF2D4890),
+                                fontSize: 11,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w500,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: ' ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 11,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w500,
+                                height: 0,
+                              ),
+                            ),
+                            TextSpan(
+                              text: 'Policy',
+                              style: TextStyle(
+                                color: Color(0xFF2D4890),
+                                fontSize: 11,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w500,
+                                height: 0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 15,
+                    top: -40,
+                    child: Container(
+                      width: 150,
+                      height: 200,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/logobk.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )
         ),
       ),
     );
