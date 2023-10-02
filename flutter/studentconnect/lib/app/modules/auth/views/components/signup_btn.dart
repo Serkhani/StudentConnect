7a4c03bb239c93bc310fada4964fd617eb3cf709
Serkhani <deviper03@gmail.com>
2023-10-02 19:17:12 +0100

    added sign up page

--- flutter/studentconnect/lib/app/modules/auth/views/components/signup_btn.dart
+++ flutter/studentconnect/lib/app/modules/auth/views/components/signup_btn.dart
@@ -5,8 +5,38 @@ class SignUpBtn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    // return ElevatedButton(
+    //   onPressed: () {},
+    //   style: ElevatedButton.styleFrom(
+    //     backgroundColor: Colors.transparent,
+    //     shape: Outline
+    //   ),
+    //   child: const Text('Sign Up'));
+    return OutlinedButton(
       onPressed: () {},
-      child: const Text('Sign Up'));
+      style: ElevatedButton.styleFrom(
+        fixedSize: const Size(double.infinity, 55.0),
+        side: const BorderSide(
+          color: Color(0xff2D4990),
+          width: 2.0
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        )
+      ),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Sign Up',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.w500,
+              color: Color(0xff2D4990),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
