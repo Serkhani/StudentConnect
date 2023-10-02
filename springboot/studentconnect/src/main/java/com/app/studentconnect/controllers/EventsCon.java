48ab42569f84780925bef2079379e1c784eb0339
Serkhani <deviper03@gmail.com>
2023-10-02 12:55:33 +0100

    initialized controllers

---
+++ springboot/studentconnect/src/main/java/com/app/studentconnect/controllers/EventsCon.java
@@ -0,0 +1,15 @@
+package com.app.studentconnect.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * EventsCon
+ */
+@RestController
+@RequiredArgsConstructor
+public class EventsCon {
+
+    
+}
