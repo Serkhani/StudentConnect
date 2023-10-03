12357b0bab686911c918964a239bc54b5774df79
Serkhani <deviper03@gmail.com>
2023-10-03 11:13:55 +0100

    displaying homepage

--- flutter/studentconnect/lib/app/modules/home/views/home_view.dart
+++ flutter/studentconnect/lib/app/modules/home/views/home_view.dart
@@ -3,20 +3,46 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
+import 'widgets/userpost.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
+      backgroundColor: const Color(
+        0xffD5F2E8,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      appBar: AppBar(
+        leading: Image.asset(
+          'assets/logobk.png',
+          height: 98.0,
+          width: 110.0,
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.person_2_outlined,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: SizedBox(
+        child: Column(
+          children: [
+            const SearchBar(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: controller.posts.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) => UserPost(post: controller.posts[index]),
+              ),
+            )
+          ],
         ),
       ),
     );
