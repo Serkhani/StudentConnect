35067e6d8dc477b9e4e7b34055dd0f097b56959e
Serkhani <deviper03@gmail.com>
2023-10-03 03:24:00 +0100

    updated textfield height

--- flutter/studentconnect/lib/app/modules/signup/views/widget/input_textfield.dart
+++ flutter/studentconnect/lib/app/modules/signup/views/widget/input_textfield.dart
@@ -24,47 +24,50 @@ class TextInputField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Obx(() => TextFormField(
-            controller: textEdCon,
-            enableSuggestions: true,
-            textCapitalization: TextCapitalization.words,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            maxLength: maxLength,
-            obscureText: obscureText.value,
-            maxLengthEnforcement: MaxLengthEnforcement.enforced,
-            buildCounter: (context,
-                {required currentLength, required isFocused, maxLength}) {
-              return null;
-            },
-            decoration: InputDecoration(
-              suffixIcon: obscureText.value
-                  ? IconButton(
-                      splashRadius: 2.0,
-                      onPressed: () {
-                        obscureText.value = !obscureText.value;
-                      },
-                      icon: obscureText.value
-                          ? const Icon(Icons.visibility)
-                          : const Icon(Icons.visibility_off),
-                    )
-                  : null,
-              filled: true,
-              labelText: labelText,
-              labelStyle: const TextStyle(
-                color: Color(0xff8391A1),
-                fontWeight: FontWeight.w500,
-                fontSize: 15.0,
+      child: Obx(() => SizedBox(
+        height: 55.0,
+        child: TextFormField(
+              controller: textEdCon,
+              enableSuggestions: true,
+              textCapitalization: TextCapitalization.words,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              maxLength: maxLength,
+              obscureText: obscureText.value,
+              maxLengthEnforcement: MaxLengthEnforcement.enforced,
+              buildCounter: (context,
+                  {required currentLength, required isFocused, maxLength}) {
+                return null;
+              },
+              decoration: InputDecoration(
+                suffixIcon: obscureText.value
+                    ? IconButton(
+                        splashRadius: 2.0,
+                        onPressed: () {
+                          obscureText.value = !obscureText.value;
+                        },
+                        icon: obscureText.value
+                            ? const Icon(Icons.visibility)
+                            : const Icon(Icons.visibility_off),
+                      )
+                    : null,
+                filled: true,
+                labelText: labelText,
+                labelStyle: const TextStyle(
+                  color: Color(0xff8391A1),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15.0,
+                ),
+                hintText: hintText,
+                hintStyle: const TextStyle(fontStyle: FontStyle.italic),
+                fillColor: const Color(0xFFF7F8F9),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                  borderSide: const BorderSide(color: Color(0xffE8ECF4)),
+                ),
               ),
-              hintText: hintText,
-              hintStyle: const TextStyle(fontStyle: FontStyle.italic),
-              fillColor: const Color(0xFFF7F8F9),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(8.0),
-                borderSide: const BorderSide(color: Color(0xffE8ECF4)),
-              ),
+              scrollPadding: const EdgeInsets.all(10.0),
             ),
-            scrollPadding: const EdgeInsets.all(10.0),
-          )),
+      )),
     );
   }
 }
